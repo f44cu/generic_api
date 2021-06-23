5d2366618e83a1748035267872377c9222db7af8
f44cu <facundoxavier2@gmail.com>
2021-06-23 10:48:56 -0300

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>API TEST</title>
+        
+    </head>
+    <body id="page-top">
+	<h1>API TEST</h1>
+    </body>
+</html>
